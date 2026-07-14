e82e16f3c76aca4730dd8307301e57e3d7711155
yzhzz00 <304443342+yzhzz00@users.noreply.github.com>
2026-07-15 02:47:01 +0800

    1

--- engine.js
+++ engine.js
@@ -7,159 +7,28 @@ this.version="V70.TEST";
 
 this.dlt=[];
 
-this.agents={};
-
 }
 
 
 
 async init(){
 
-
-await this.loadData();
-
+console.log("AIEngine init运行");
 
 return true;
 
-
 }
-
-
-
-
-async loadData(){
-
-
-try{
-
-
-let res=await fetch(
-"data/dlt.txt?t="+Date.now()
-);
-
-
-
-if(!res.ok){
-
-throw new Error(
-"dlt.txt读取失败"
-);
-
-}
-
-
-
-let text=await res.text();
-
-
-
-let lines=text.trim().split(/\n+/);
-
-
-
-this.dlt=[];
-
-
-
-lines.forEach(line=>{
-
-
-let arr=line.trim().split(/\s+/);
-
-
-
-if(arr.length>=8){
-
-
-this.dlt.push({
-
-
-front:[
-
-arr[2],
-arr[3],
-arr[4],
-arr[5],
-arr[6]
-
-],
-
-
-back:[
-
-arr[7],
-arr[8]
-
-]
-
-
-});
-
-
-}
-
-
-
-});
-
-
-
-console.log(
-"数据数量:",
-this.dlt.length
-);
-
-
-
-}catch(e){
-
-
-console.log(
-"数据错误:",
-e
-);
-
-
-
-throw e;
-
-
-}
-
-
-
-}
-
-
-
-
-
-loadAgents(){
-
-
-
-return true;
-
-
-}
-
 
 
 
 
 status(){
 
-
 return {
-
 
 version:this.version,
 
-
-data:this.dlt.length,
-
-
-agents:[]
+data:this.dlt.length
 
 };
 
@@ -172,6 +41,4 @@ agents:[]
 
 
 
-
-
-window.AIEngine=new AIEngine();
+window.AIEngine = new AIEngine();
