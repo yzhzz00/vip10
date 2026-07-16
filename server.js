f3bdad502720e344a57638f2427c1d067a7edf6a
yzhzz00 <304443342+yzhzz00@users.noreply.github.com>
2026-07-16 14:26:05 +0800

    1

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 
-const runAI = require("./app");
+
+const app = express();
 
 
-const server = express();
 
-
-server.use(cors());
-
-server.use(express.json());
-
-
-// 加载网页目录
-
-server.use(
+app.use(
     express.static(
         path.join(
             __dirname,
@@ -25,9 +16,8 @@ server.use(
 );
 
 
-// 首页
 
-server.get("/", (req,res)=>{
+app.get("/",(req,res)=>{
 
     res.sendFile(
 
@@ -42,52 +32,18 @@ server.get("/", (req,res)=>{
 });
 
 
-// 测试接口
-
-server.get(
-    "/api/test",
-    (req,res)=>{
-
-        res.json({
-
-            status:"ok",
-
-            version:"V1.0.0"
-
-        });
-
-    }
-);
-
-
-// AI接口
-
-server.get(
-    "/api/analyze",
-    (req,res)=>{
-
-        res.json(
-
-            runAI()
-
-        );
-
-    }
-);
-
-
 
 const PORT =
 process.env.PORT || 3000;
 
 
 
-server.listen(
+app.listen(
     PORT,
     ()=>{
 
         console.log(
-            "DLT-AI CORE V1.0.0 running"
+            "SERVER OK"
         );
 
     }
